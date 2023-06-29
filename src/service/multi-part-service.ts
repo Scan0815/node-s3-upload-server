06f8254544ab63412a9d87837c784e067a54b99c
oliver <oliver@bitcaster.de>
2023-06-29 15:09:38 +0200

    remove event

--- src/service/multi-part-service.ts
+++ src/service/multi-part-service.ts
@@ -110,7 +110,6 @@ export class MultiPartService {
 
             }
 
-
             if (fileType.includes("image")) {
                 pathObj = {
                     images : `${transferId}/images/${fileId}`
